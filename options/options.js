d64eb4bd961b42ddadb26890ebc0c4e1510b6806
Clemens van der Walt <cvdwalt@methys.com>
2023-06-14 18:55:26 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "testo-41482",
-  url: "https://testo-41482.botics.co",
-  api: "https://testo-41482.botics.co/api/v1"
-}
+  "name": "testo-41482",
+  "url": "https://testo-41482.botics.co",
+  "api": "https://testo-41482.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
